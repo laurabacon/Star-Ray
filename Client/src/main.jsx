74f5216badf726bf3a3bd665955d9c8c8fbce916
EChrist01 <146894896+EChrist01@users.noreply.github.com>
2024-02-09 11:39:04 -0700

    html -> react, import/refactor (#12)

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,8 +3,102 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function Header() {
+  return (
+    <header id="nav">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <h1 className="title">STAR RAY DESIGN</h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" id="home-button" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="shop-button" href="#">
+                  Shop
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="cart-button" href="#">
+                  My Cart
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+// function Main({ body }) {
+//   return <main id="main">{body}</main>;
+// }
+
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <h6>Contact Us</h6>
+            <p className="text-justify">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div className="col-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul id="footer-list">
+              <li>
+                <a href="#" id="shop-footer">
+                  Shop
+                </a>
+              </li>
+              <li>
+                <a href="#" id="cart-footer">
+                  My Cart
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main body={<div>Content goes here</div>} /> {}
+      <Footer />
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+export default App;
