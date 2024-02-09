2a15ae8a6ffe9e1ff154b02001ff54000c6647a6
laurabacon <150396874+laurabacon@users.noreply.github.com>
2024-02-09 10:07:39 -0700

    Brown (#9)
    * directories and dependecy
    
    * set up
    
    * schemas, resolvers, and typeDef
    
    * config connection -- still needs db name added
    
    * delete node
    
    * profile models
    
    ---------
    
    Co-authored-by: EChrist01 <146894896+EChrist01@users.noreply.github.com>

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -18,16 +18,28 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect email or password');
+      try {
+        if (!name || !email || !password) {
+          throw new Error("All fields are required.");
+        }
+        if (password.length < 5) {
+          throw new Error("Password must be at least 5 characters long.");
+        }
+        const user = await User.create({ name, email, password });
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        throw new Error(error.message);
       }
+    },
+
+    login: async (parent, { email, password }) => {
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new AuthenticationError('Incorrect email or password');
+        }
 
       const correctPassword = await user.isCorrectPassword(password);
 
@@ -37,16 +49,25 @@ const resolvers = {
 
       const token = signToken(user);
       return { token, user };
-    },
-    logout: async (parent, args, context) => {
-      if (context.user) {
-        // Perform any necessary logout actions
-        // For example, you can invalidate the token or remove it from the client-side storage
-        return { message: 'Logged out successfully' };
-      }
-      throw new AuthenticationError('Not logged in');
-    },
+    } catch (error) {
+    throw new Error(error.message);
+    }
   },
-};
+    logout: async (parent, args, context) => {
+      try {
+        if (context.user) {
+          return { message: 'Logged out successfully' };
+        }
+        throw new AuthenticationError('Not logged in');
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+      addProductToCart: async (parent, { productId }, context) => {
+        if (!context.user) {
+          throw new AuthenticationError('You must be logged in to add products to your cart.');
+        }
+      }
+  }};
 
 module.exports = resolvers;
