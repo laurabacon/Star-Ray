ee13a79488bc395ece18af6cdb9f5aa2c4e7f540
Bryncidently <email@brynglotfelty.com>
2024-02-09 13:41:01 -0500

    added jsx files

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Cart from './pages/Cart.jsx'
+import ProductsPage from './pages/Shop.jsx'
+import HomePage from '../component/HomePage.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
---
+++ Client/src/pages/Shop.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+const Shop = () => {
+  
+
+  return (
+    <section>
+      <h1>PRODUCTS</h1>
+      <div className="displayCart"></div>
+      <section className="products">
+        {products.map((product) => (
+          <div key={product.id} className="col-md-4">
+            <div className={`card ${product.type}`} style={{ width: '18rem' }}>
+              <img src={product.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">{`${product.scent} - ${product.size}`}</p>
+                <p>${product.price}</p>
+                <button data-id={product.id} id="cartAdd" className="btn btn-primary">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </section>
+  );
+};
+
+export default Shop;
