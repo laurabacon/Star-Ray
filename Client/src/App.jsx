1c211752ad7b08aff407692582136e3f7dad6c6d
Bryn Glotfelty <150275734+Bryncidently@users.noreply.github.com>
2024-02-09 13:50:09 -0500

    added jsx files (#14)
    Co-authored-by: Bryncidently <email@brynglotfelty.com>
    Co-authored-by: EChrist01 <146894896+EChrist01@users.noreply.github.com>

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import HomePage from '../component/HomePage';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+ 
+  const renderPage = () => {
+    const currentPath = window.location.pathname;
+
+    if (currentPath === '/shop') {
+      return <Shop />;
+    } else if (currentPath === '/cart') {
+      return <Cart />;
+    } else {
+      return <HomePage />;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {renderPage()}
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+
+
+
+
