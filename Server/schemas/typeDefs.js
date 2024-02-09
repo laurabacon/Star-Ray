0207ea28a9e7e060d60b247f3e813cd4767a2053
laurabacon <laura_kessinger@yahoo.com>
2024-02-09 10:36:18 -0700

    typeDef

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -6,6 +6,13 @@ const typeDefs = `
     password: String
   }
 
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    price: Float
+  }
+
   type Auth {
     token: ID!
     user: User
@@ -14,12 +21,15 @@ const typeDefs = `
   type Query {
     user: User
     me: User
+    getProduct(productId: ID!): Product
+    getAllProducts: [Product]
   }
 
   type Mutation {
     addUser(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     logout: Message
+    addProductToCart(productId: ID!): Message
   }
 
   type Message {
