2de10e1dbf75ae21733f6d25cfeebacdf79d6d49
Halden Kavanagh <haldenkav@gmail.com>
2024-02-12 12:56:30 -0700

    added react router for navigation

--- Client/src/components/pages/Shop.jsx
+++ Client/src/components/pages/Shop.jsx
@@ -1,14 +1,33 @@
+import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 
 export default function Shop() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
   const products = data?.getAllProducts || [];
-  console.log(products);
+  
 
   return (
     <div>
       <h1>PRODUCTS</h1>
-    </section>
+      <main>
+        <div>
+          <div>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <div>
+                <h2>All Scents:</h2>
+                <ul>
+                  {products.map((product) => (
+                    <li key={product._id}>{product.scent}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
