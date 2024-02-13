ab64e22fee8188074c138f813876a433fdceef66
HaldenKavanagh <150835854+HaldenKavanagh@users.noreply.github.com>
2024-02-13 10:18:10 -0700

    Kavanagh (#45)
    * added product cards to shop page as well as filter buttons to view items based on type
    
    * buttons working
    
    * added package-lock.json to gitignore
    
    * got server to run
    
    ---------
    
    Co-authored-by: Halden Kavanagh <haldenkav@gmail.com>

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -28,10 +28,7 @@ const router = createBrowserRouter([
         path: "/Shop",
         element: <Shop />,
       },
-      {
-        path: "/Login",
-        element: <Login />,
-      },
+
       //   {
       //     path: "/Login",
       //     element: <Login />,
