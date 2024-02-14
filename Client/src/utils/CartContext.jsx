9cb95e135ec453ab23ec17b1fb604cf0cd138547
Bryncidently <email@brynglotfelty.com>
2024-02-13 20:41:07 -0500

    Cart rendering works... kinda

---
+++ Client/src/utils/CartContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer, useContext } from 'react';
+
+const CartContext = createContext();
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+  
+    default:
+      return state;
+  }
+};
+
+export const CartProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, { items: [] });
+
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
